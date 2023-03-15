e30289d3f59fd48f0b4e5079e9e048736aa0c8dd
nlightmarecx <lfromwhere@gmail.com>
2023-03-16 00:00:18 +0400

    modify to grid css from nested loop

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
 const panel_Div = document.getElementById("panel_Div");
-const firstCol = document.getElementById("panelCol_Div0");
+const panelSquare_Div = document.getElementsByClassName("panelColRow_Div");
 
-const PANEL_WIDTH = 180;
-const PANEL_HEIGHT = 360;
+const PANEL_WIDTH = 300;
+const PANEL_HEIGHT = PANEL_WIDTH*2;
 
 const panelWidth = document.getElementById("panel_Div").style.width = PANEL_WIDTH+"px";
 const panelHeight = document.getElementById("panel_Div").style.height = PANEL_HEIGHT+"px";
-const toolsWidth = document.getElementById("tools_Div").style.width = PANEL_WIDTH/1.25+"px";
+const toolsWidth = document.getElementById("tools_Div").style.width = PANEL_WIDTH/1.6+"px";
 
 
-let SIZE_BOARD_X = 6;
+let SIZE_BOARD_X = 8;
 let SIZE_BOARD_Y = SIZE_BOARD_X*2;
+let COLOR_BORDER = "rgba(211, 211, 211, 0.25)";
 let SIZE_BORDER = 1;
 
 let SIZE_SQUARE_X = PANEL_WIDTH/SIZE_BOARD_X-SIZE_BORDER;
-let SIZE_SQUARE_Y = PANEL_HEIGHT/(SIZE_BOARD_Y)-SIZE_BORDER;
+let SIZE_SQUARE_Y = PANEL_HEIGHT/SIZE_BOARD_Y-SIZE_BORDER;
+
+panel_Div.style.gridTemplateColumns = `repeat(${SIZE_BOARD_X}, 1fr)`;
+panel_Div.style.gridTemplateRows = `repeat(${SIZE_BOARD_Y}, 1fr)`;
 
 //END OF Panel DIV
 //................................................
 
 //................................................
 //START OF TOOLS DIV
-const gridOff_Btn = document.getElementById("#gridLin_Off");
+const inkColor = document.getElementById("inkColor");
+const gridLinesOff = document.getElementById("#gridLinesOff");
 
+let currentInkColor = "#0000ff";
 
+//......................Canvas Panel..............................
+//....................................................MAKE BOARD DIVs
 createBoard();
 
+function createBoard(){
+    for(let i=0; i<SIZE_BOARD_X*SIZE_BOARD_Y; i++){
+        const panelColRow_Div = document.createElement("div");
+        panelColRow_Div.setAttribute("id", "panelColRow_Div"+i)
+        panelColRow_Div.setAttribute("class", "panelColRow_Div");
+        panel_Div.insertAdjacentElement("beforeend", panelColRow_Div);
+    }
+}
+
+/*
 function createBoard(){
     for(let i=0; i<SIZE_BOARD_X; i++){
         const panelCol_Div = document.createElement("div");
@@ -43,27 +61,25 @@ function createBoard(){
     }
 }
 
-const topLeftCorner = document.getElementById("panelColRow_Div0-0");
-const topRightCorner = document.getElementById("panelColRow_Div"+(SIZE_BOARD_X-1)+"-0");
-const bottomRightCorner = document.getElementById("panelColRow_Div"+(SIZE_BOARD_X-1)+"-"+(SIZE_BOARD_Y-1));
-const bottomLeftCorner = document.getElementById("panelColRow_Div0-"+(SIZE_BOARD_Y-1));
+*/
+
+//....................................................ROUND THE CORNERS
+const topLeftCorner = document.getElementById("panelColRow_Div0");
+const topRightCorner = document.getElementById("panelColRow_Div"+(SIZE_BOARD_X-1));
+const bottomRightCorner = document.getElementById("panelColRow_Div"+(SIZE_BOARD_X*SIZE_BOARD_Y-1));
+const bottomLeftCorner = document.getElementById("panelColRow_Div"+(SIZE_BOARD_X*SIZE_BOARD_Y-SIZE_BOARD_X));
 
 topLeftCorner.style.borderRadius = "20% 0 0 0";
 topRightCorner.style.borderRadius = "0 20% 0 0";
 bottomRightCorner.style.borderRadius = "0 0 20% 0";
 bottomLeftCorner.style.borderRadius = "0 0 0 20%";
 
-
-/*
-$('#panel_Div').mouseenter(function(){
-    $('#panel_Div').css("cursor", "none");
-}) 
-*/
+//....................................................GHOST HOVER + DRAW ON MOUSE DOWN
 
 $('.panelColRow_Div').on({
     mousedown: function(){
         $('.panelColRow_Div').hover(function(){
-            $(this).css("background-color", "blue");
+            $(this).css("background-color", currentInkColor);
         });
     },
     mouseup: function(){
@@ -73,27 +89,55 @@ $('.panelColRow_Div').on({
     }
 })
 
-function myFunction(){
-    $('.panelColRow_Div').hover(function(){
-        $(this).css("background-color", "blue");
-    });
+
+/*
+
+panelSquare_Div.addEventListener('mouseover', ghostHover)
+ghostHover();
+function ghostHover(e){
+    e.target.classList.add('hover');
+    e.target.addEventListener('transitionend',() => e.target.classList.remove('hover'));
+
 }
+
+$('#panel_Div').mouseenter(function(){
+    $('#panel_Div').css("cursor", "none");
+}) 
+*/
 
 
 //......................Tools Panel..............................
-//....................................................MAKE BOARD
-$('.panelColRow_Div').css({"borderLeft": "solid "+ SIZE_BORDER + "px rgba(211, 211, 211, 0.25)",
-"borderTop": "solid "+ SIZE_BORDER + "px rgba(211, 211, 211, 0.25)"});
+//....................................................INITIAL BOARD
+$('.panelColRow_Div').css({"borderLeft": `solid ${SIZE_BORDER}px ${COLOR_BORDER}`,
+"borderTop": `solid ${SIZE_BORDER}px ${COLOR_BORDER}`, "borderRight": "none", "borderBottom": "none"});
 $('.panelColRow_Div').css({"height": SIZE_SQUARE_X+"px", "width":
 SIZE_SQUARE_Y+"px"});
-$('#panelCol_Div0').css({"borderLeft": "0"});
 
 //....................................................INK COLOR
 $('#ink_Div').click(function(){ 
     $('.panelColRow_Div').hover(function(){
-        $(this).css("background-color", "blue");
+        //currentInkColor = "#0000ff"; //needs work: should bring back the original color
+        $(this).css("background-color", currentInkColor);
     });
 })
+
+inkColor.addEventListener('input', changeInkColor);
+function changeInkColor(e){
+    currentInkColor = e.target.value;
+}
+
+//....................................................RAINBOW COLOR
+$('#rainbow_Div').click(function(){ 
+    $('.panelColRow_Div').hover(function(){
+        //$(this).css("background-color", makeRainbowColor);
+    });
+})
+
+function makeRainbowColor(){
+    rainbowColor = Math.floor(Math.random()*0xffffff).toString(16);
+    console.log(rainbowColor);
+    currentInkColor = "#"+rainbowColor;
+}
 
 //....................................................RUBBER
 $('#rubber_Div').click(function(){ 
@@ -120,10 +164,9 @@ $('#gridLinesOn_Div').click(function(){
 function removeGridLines(x){
     SIZE_BORDER = x;
     SIZE_SQUARE_X = PANEL_WIDTH/SIZE_BOARD_X-SIZE_BORDER;
-    SIZE_SQUARE_Y = PANEL_HEIGHT/(SIZE_BOARD_Y)-SIZE_BORDER;
-    $('.panelColRow_Div').css({"borderLeft": "solid "+ SIZE_BORDER + "px rgba(211, 211, 211, 0.25)",
-    "borderTop": "solid "+ SIZE_BORDER + "px rgba(211, 211, 211, 0.25)"});
-    $('.panelColRow_Div').css({"height": SIZE_SQUARE_X+"px", "width":
-    SIZE_SQUARE_Y+"px"});
-    $('#panelCol_Div0').css({"borderLeft": "0"});
+    SIZE_SQUARE_Y = PANEL_HEIGHT/SIZE_BOARD_Y-1;
+    $('.panelColRow_Div').css({"borderLeft": `solid ${SIZE_BORDER}px ${COLOR_BORDER}`,
+    "borderTop": `solid ${SIZE_BORDER}px ${COLOR_BORDER}`, "borderRight": "none", "borderBottom": "none"});
+    $('.panelColRow_Div').css({"height": `${SIZE_SQUARE_X}px`, "width":
+    `${SIZE_SQUARE_Y}px`});
 }
